58ee927990b48259cf8ddc5b7b9df99169bd8355
Dewonoto20 <dewonoto57@gmail.com>
2021-04-02 11:06:29 +0700

    Upload Latihan Pertemuan 6

---
+++ Pertemuan 6/Pert6_latihan1.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$a[0] = 1;
+	$a[1] = 3;
+	$a[2] = 5;
+	$jumlah = count($a);
+	print "Jumlah Array a = $jumlah <br>";
+	$b["buah"] = "semangka";
+	$b["sayur"] = "wortel";
+	$b["daging"] = "ayam";
+	$b["utama"] = "nasi";
+	$jumlah = sizeof($b);
+	print "Jumlah array b = $jumlah <br>";
+
+	?> 
